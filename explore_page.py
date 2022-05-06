57c61d79967d2bcdcb75c095c0a67877be60a017
enriquegv001 <enriquegv001@gmail.com>
2022-05-06 14:29:12 -0500

    initial comit

---
+++ explore_page.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+
+@st.cache
+def load_data():
+    df = pd.read_csv("dfexplore (1).csv")
+    #df = df[['Especialidad', 'Operaciones-Calidad', 'MTTO-DIMA', 'Comercial-Planeamiento', 'DIGI-SC','Resto-Soft', 'Apto/No Apto',	'Destacado Pym.1',	'Ingles']]
+    return df
+
+df = load_data()
+
+def show_explore_page():
+    
+    image = Image.open('Ternium.png')
+    st.image(image, width = 150)
+    
+    st.title("Análisis descrptivo Candidatos Ingresados")
+    
+    st.write(
+        """ #### Gráfico especialidades """
+    )
+    
+    data = df["Especialidad"].value_counts()
+    
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot(fig1)
+
+    st.write("""#### Gráfica número de semestres de los candidatos""")
+    
+    data = df['Avance'].value_counts().sort_values(ascending=True)
+    st.bar_chart(data)
+
+    st.write("""#### Gráfica de recomendacion en cada area""")
+    
+    recom = ['Not Recommended', 'Recommended', 'Highly Recommended']
+    d_op = [df['Operaciones-Calidad'][df['Operaciones-Calidad']=='Do Not Recommend'].count(), df['Operaciones-Calidad'][df['Operaciones-Calidad']=='Recommend'].count(), df['Operaciones-Calidad'][df['Operaciones-Calidad']=='Highly Recommend'].count()]
+    d_mtto = [df['MTTO-DIMA'][df['MTTO-DIMA']=='Do Not Recommend'].count(), df['MTTO-DIMA'][df['MTTO-DIMA']=='Recommend'].count(), df['MTTO-DIMA'][df['MTTO-DIMA']=='Highly Recommend'].count()]
+    d_com = [df['Comercial-Planeamiento'][df['Comercial-Planeamiento']=='Do Not Recommend'].count(), df['Comercial-Planeamiento'][df['Comercial-Planeamiento']=='Recommend'].count(), df['Comercial-Planeamiento'][df['Comercial-Planeamiento']=='Highly Recommend'].count()]
+    d_digi = [df['DIGI-SC'][df['DIGI-SC']=='Do Not Recommend'].count(), df['DIGI-SC'][df['DIGI-SC']=='Recommend'].count(), df['DIGI-SC'][df['DIGI-SC']=='Highly Recommend'].count()]
+    d_resto = [df['Resto-Soft'][df['Resto-Soft']=='Do Not Recommend'].count(), df['Resto-Soft'][df['Resto-Soft']=='Recommend'].count(), df['Resto-Soft'][df['Resto-Soft']=='Highly Recommend'].count()]
+
+    x_axis = np.arange(len(recom))
+
+    # Multi bar Chart
+    
+    fig = plt.figure(figsize = (15, 8))
+
+
+    plt.bar(x_axis -0.3, d_op, width=0.15, label = 'Operaciones-calidad')
+    plt.bar(x_axis -0.15, d_mtto, width=0.15, label = 'MTTO-DIMA')
+    plt.bar(x_axis +0, d_com, width=0.15, label = 'Comercial-Planeamiento')
+    plt.bar(x_axis +0.15, d_digi, width=0.15, label = 'DIGI-SC')
+    plt.bar(x_axis +0.3, d_resto, width=0.15, label = 'Resto-Soft')
+
+    # Xticks
+
+    plt.xticks(x_axis, recom)
+
+    # Add legend
+
+    plt.legend()
+
+    # Display
+    st.pyplot(fig)
+    
+    
+    st.write(
+        """ #### Actividad grupal """
+    )
+    
+    data = df["Destacado Pym.1"].value_counts()
+    
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot(fig1)
---
+++ predict_page.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import numpy as np
+import sklearn
+from PIL import Image
+from sklearn.ensemble import GradientBoostingClassifier
+import joblib
+
+def load_model():
+    with open('model.pkl', 'rb') as file:
+        data = pickle.load(file)
+        
+    return data
+        
+
+data = load_model()
+
+def show_predict_page():
+        
+    image = Image.open('Ternium.png')
+    st.image(image, width = 150)
+    
+    st.title("Predicción de Reclutamiento")
+    
+    st.write("""### Selecciona las características del candidato a evaluar""")
+    
+    st.write("""##### Operaciones-Calidad, MTTO-DIMA, Comercial-Planeamiento, DIGI-SC, Resto-Soft:\n
+            .       Highly Recommend = 2\n
+            Recommend = 1\n
+            Do Not Recommend = 0\n""")
+    OperacionCalidad = (0,1,2)
+    OperacionCalidad = st.selectbox("Operaciones-Calidad ", OperacionCalidad)
+
+    MTTO = (0,1,2)
+    MTTO = st.selectbox("MTTO-DIMA", MTTO)
+    
+    Comercial = (0,1,2)
+    Comercial = st.selectbox("Comercial-Planeamiento", Comercial)
+    
+    DIGI = (0,1,2)
+    DIGI = st.selectbox("DIGI-SC", DIGI)
+
+    RESTO = (0,1,2)
+    RESTO = st.selectbox("Resto-Soft", RESTO)
+    
+    st.write("""##### Ingles:\n
+             .        A1 = 2\n
+             A2 = 3\n
+             B1 = 4\n
+             B2 = 5\n
+             C1 = 6\n
+             C2 = 7\n""")
+    Ingles = (0,1,2,3,4,5,6,7)
+    Ingles = st.selectbox("Ingles", Ingles)
+    
+    ok = st.button("Ingreso")
+    if ok:
+        X = pd.Series([OperacionCalidad, MTTO, Comercial, DIGI, RESTO, Ingles])      
+        
+        ingreso = data.predict([X])
+        st.subheader(f"""¿El candidato ingresa?:\n
+                     (Si = 1; No = 0)\n
+                     {ingreso[0]}""")
